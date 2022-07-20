53819fff2a6575e19f8f4ac4c6349e430f378d08
sangwoong <sangwoong03@gmail.com>
2022-07-20 17:52:44 +0900

    Update: version Update

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 - [공식문서 en](https://www.electronjs.org/)
 - [한글 버전](https://tinydew4.github.io/electron-ko)
-- [Youtube](https://www.youtube.com/watch?v=3yqDxhR2XxE&t=518s)
+- [참고용 블로그](https://programmerk.tistory.com/55)
 
 ## ✅ Electron Tutorial
 
@@ -14,7 +14,7 @@
 
 - 기존의 웹앱이 있다면 `.exe` 확장자 명의 실행 파일로 데스크탑 앱 생성 가능
 
-## ✅ 초기 세팅
+## ✅ 초기 세팅 :: Only Electron
 
 ### 📌 1. npx
 
@@ -68,87 +68,4 @@ npm start
 <br>
 <br>
 
-## ✅ Process
-
-- 일렉트론은 `chromium`의 다중 프로세스 아키텍처를 상속
-
-- 웹 브라우저와 비슷한 구조
-
-- 웹 브라우저는 구조적으로 복잡한 애플리케이션으로서,  
-  하나의 브라우저가 여러 탭을 관리하거나 써드파티 (third-party) 익스텐션을 로드하는 등  
-  컨텐츠를 화면에 표시하는 것 외에도 부수적인 기능 존재
-
-### 📌 Single Process
-
-- 초기의 브라우저는 이 모든 기능을 하나의 프로세스로 처리
-- 문제점: 웹 사이트 하나를 종료해도 전체 브라우저에 영향
-
-### 📌 Multi Process
-
-- `Chrome`에서 각각의 탭을 별도의 프로세스에서 렌더링
-- 하나의 웹사이트에서 오류가 발생해도 전체 브라우저에 영향을 미치지 않음.
-
-<br>
-
-일렉트론 어플리케이션도 이와 유사하고 `Main`과 `Renderer` 두 유형의 프로세스를 관리하여 개발을 진행할 것입니다.
-
-<br>
-
-### 📌 Main Process
-
-<br>
-
-### 📌 Rendere Process
-
-<br>
-<br>
-
-## Tutorial: nativeTheme
-
-사용자 임의로 인터페이스를 바꾸는 방법에 대한 튜토리얼 소개
-
-### 📌 다크모드/라이트모드 전환
-
-1. Main:: `natvieTheme` 모듈의 `themeSource` 속성 사용하기
-
-   - 설정한 `themeSource` 속성 값은 Renderer 프로세스로 전달
-   - CSS 파일에서 `prefers-color-scheme` 미디어 쿼리를 통해 UI 변경
-   - `ipcMain.handle()`을 사용하여 Renderer 프로세스에서 `dark-mode` 채널로 보내는 메시지 수신
-     <br>
-
-2. Preload:: `window` 객체에 `darkMode`라는 새로운 API 추가
-
-   - dark-mode:toggle : `dark-mode` 채널에 `toggle` 메서드 할당
-   - dark-mode:system : `dark-mode` 채널에 `system` 메서드 할당
-   - `ipcRenderer.invoke()`을 사용하여 `dark-mode` 채널을 통해 Renderer 프로세스에서 Main 프로세스로 메시지 송신
-     <br>
-
-3. Rendere:: `button` 태그 이벤트 리스너 추가
-
-   - 이벤트리스너를 통해 `HTML` 태그에 이벤트 추가
-   - 각각의 이벤트를 통해 `window.darkMode`의 `toggle()`, `system()` 메서드를 호출
-
-4. Dark/Light 모드 전환은 잘 되나 system 버튼을 눌렀을 때 다음 에러 발생
-
-```
-Uncaught (in promise) Error:
-Error invoking remote method 'dark-mode:system':
-No handler registered for 'dark-mode:system'
-```
-
-아직 에러 원인 모르겠으나
-
-https://runebook.dev/ko/docs/electron/api/native-theme 참조에 의하면,
-
-- `nativeTheme.themeSource​` 속성의 값으로 system, light, dark 세가지를 가질 수 있음.
-
-1. `system` 설정 시 모든 것이 OS 기본 값으로 재설정.
-
-2. `dark`로 설정 시 모든 UI가 어두운 UI로 변경.
-
-   - `nativeTheme.shouldUseDarkColors ` === true
-   - 만약 렌더링되는 화면을 바꾸기 위해서는 CSS 파일에서 미디어쿼리 사용해야함
-
-3. `light`로 설정 시 모든 UI가 밝은 UI로 변경.
-   - `nativeTheme.shouldUseDarkColors ` === false
-   - 만약 렌더링되는 화면을 바꾸기 위해서는 CSS 파일에서 미디어쿼리 사용해야함
+## ✅ 초기 세팅 :: Vue on Electron
--- src/background.js
+++ src/background.js
@@ -1,81 +1,139 @@
-'use strict'
+import { app, protocol, BrowserWindow, ipcMain } from "electron";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
+import installExtension, { VUEJS3_DEVTOOLS } from "electron-devtools-installer";
+const isDevelopment = process.env.NODE_ENV !== "production";
+const log = require("electron-log");
+//update 추가
+import { autoUpdater } from "electron-updater";
 
-import { app, protocol, BrowserWindow } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+let win;
+let mode = process.env.VUE_APP_MODE; // 실행 모드
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
-])
+	{ scheme: "app", privileges: { secure: true, standard: true } },
+]);
 
 async function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
-    }
-  })
+	// Create the browser window.
+	win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			// Use pluginOptions.nodeIntegration, leave this alone
+			// See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+			nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+			contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
+		},
+	});
 
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
-  } else {
-    createProtocol('app')
-    // Load the index.html when not in development
-    win.loadURL('app://./index.html')
-  }
+	if (process.env.WEBPACK_DEV_SERVER_URL) {
+		// Load the url of the dev server if in development mode
+		await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+		if (!process.env.IS_TEST) win.webContents.openDevTools();
+	} else {
+		createProtocol("app");
+		// Load the index.html when not in development
+		win.loadURL("app://./index.html");
+	}
 }
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on("window-all-closed", () => {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+app.on("activate", () => {
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS_DEVTOOLS)
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
-    }
-  }
-  createWindow()
-})
+
+app.on("ready", async () => {
+	if (isDevelopment && !process.env.IS_TEST) {
+		// Install Vue Devtools
+		try {
+			await installExtension(VUEJS3_DEVTOOLS);
+		} catch (e) {
+			console.error("Vue Devtools failed to install:", e.toString());
+		}
+	}
+	createWindow();
+
+	log.info("app.on ready");
+	let result = await autoUpdater.getUpdateInfoAndProvider();
+	let releasesVersion = result.info.version;
+	log.info(releasesVersion);
+	log.info(releasesVersion.split("-")[1]);
+	if (releasesVersion.split("-")[1] === mode) {
+		log.info(mode);
+		autoUpdater.checkForUpdates();
+	}
+});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit()
-      }
-    })
-  } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
-  }
+	if (process.platform === "win32") {
+		process.on("message", (data) => {
+			if (data === "graceful-exit") {
+				app.quit();
+			}
+		});
+	} else {
+		process.on("SIGTERM", () => {
+			app.quit();
+		});
+	}
 }
+
+// HelloWorld.vue 안의 ipcRender.send 를 통해 호출되면 app_version을 확인한 후 HelloWorld.vue 로 보냄
+ipcMain.on("app_version", (event) => {
+	log.info("app_version");
+
+	event.sender.send("app_version", {
+		version: app.getVersion(),
+	});
+});
+// HelloWorld.vue 안의 ipcRender.send 를 통해 호출되면 앱을 종료하고 install을 시작함
+ipcMain.on("restart_app", () => {
+	log.info("restart_app");
+	autoUpdater.quitAndInstall();
+});
+
+// 업데이트 오류시
+autoUpdater.on("error", function (error) {
+	log.info("restart_app", error);
+	win.webContents.send("error");
+});
+
+// 업데이트 체크
+autoUpdater.on("checking-for-update", () => {
+	log.info("checking-for-update");
+	win.webContents.send("checking-for-update");
+});
+
+// 업데이트할 내용이 있을 때
+autoUpdater.on("update-available", () => {
+	log.info("A new update is available");
+	win.webContents.send("update-available");
+});
+
+// 업데이트할 내용이 없을 때
+autoUpdater.on("update-not-available", () => {
+	log.info("update-not-available");
+	win.webContents.send("update-not-available");
+});
+
+//다운로드 완료되면 업데이트
+autoUpdater.on("update-downloaded", () => {
+	log.info("update-downloaded");
+	win.webContents.send("update-downloaded");
+});
