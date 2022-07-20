a12bc18a6d6915779bd5ef3d4cc51dbba81c44d6
sangwoong <sangwoong03@gmail.com>
2022-07-20 16:54:28 +0900

    Add: vue cli

--- src/main.js
+++ src/main.js
@@ -1,69 +1,8 @@
-const { app, BrowserWindow, ipcMain, nativeTheme } = require("electron");
-const path = require("path");
+import Vue from 'vue'
+import App from './App.vue'
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-// eslint-disable-next-line global-require
-if (require("electron-squirrel-startup")) {
-	app.quit();
-}
+Vue.config.productionTip = false
 
-const createWindow = () => {
-	// Create the browser window.
-	const mainWindow = new BrowserWindow({
-		width: 400,
-		height: 300,
-		webPreferences: {
-			nodeIntegration: true,
-			enableRemoteModule: true,
-			preload: path.join(__dirname, "preload.js"),
-		},
-	});
-
-	// and load the index.html of the app.
-	mainWindow.loadFile(path.join(__dirname, "index.html"));
-
-	// Open the DevTools.
-	// mainWindow.webContents.openDevTools();
-
-	// dark-mode
-	// ipcMain.handle("dark-mode:toggle", () => {
-	// 	if (nativeTheme.shouldUseDarkColors) {
-	// 		nativeTheme.themeSource = "light";
-	// 	} else {
-	// 		nativeTheme.themeSource = "dark";
-	// 	}
-	// 	return nativeTheme.shouldUseDarkColors;
-	// });
-
-	// ipcMain.handle("dark-mode:system", () => {
-	// 	nativeTheme.themeSource = "system";
-	// });
-	ipcMain.handle("timer", (event, data) => {
-		console.log("start");
-	});
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
-});
-
-app.on("activate", () => {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (BrowserWindow.getAllWindows().length === 0) {
-		createWindow();
-	}
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
